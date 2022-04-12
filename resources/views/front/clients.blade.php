3af5f6d67d1855d0c9cc1c7758615e717767f38d
exeibier <xabierhh@gmail.com>
2022-04-12 12:01:51 -0500

    fix: minor changes in clients

--- resources/views/front/clients.blade.php
+++ resources/views/front/clients.blade.php
@@ -30,6 +30,48 @@
                         <div class="row">
                             <div class="col-lg-2">
                                 <div class="animation-block reveal-down-1">
+                                    <div class="item"><b>BAYER</b></div>
+                                    <div class="item"><b>BMW</b></div>
+                                    <div class="item"><b>Church and Dwight</b></div>
+                                    <div class="item">Flawless</div>
+                                    <div class="item">Batiste</div>
+                                    <div class="item">Waterpick</div>
+                                    <div class="item">Sterimar</div>
+                                    <div class="space"></div>
+                                    <div class="item"><b>DECATHLON</b></div>
+                                    <div class="item"><b>DIAGEO</b></div>
+                                    <div class="item">Johnnie Walker</div>
+                                    <div class="item">Smirnoff</div>
+                                    <div class="item">Buchanan’s </div>
+                                    <div class="item">Baileys</div>
+                                    <div class="item">Black & White</div>
+                                    <div class="space"></div>
+                                    <div class="item"><b>FONDEADORA</b></div>
+                                    <div class="item"><b>Grupo Modelo</b></div>
+                                    <div class="item">Corona</div>
+                                    <div class="item">Cerveza Modelo</div>
+                                    <div class="item">Michelob Ultra</div>
+                                    <div class="item">Stella Artois</div>
+                                    <div class="item">Becks</div>
+                                    <div class="item">Michelob Seltzer</div>
+                                    <div class="item">Pacifico</div>
+                                    <div class="item">Victoria</div>
+                                    <div class="item">Z Tech</div>
+                                    <div class="item">Craft Beer Portfolio</div>
+                                    <div class="space"></div>
+                                </div>
+                            </div>
+                            <div class="col-lg-2">
+                                <div class="animation-block reveal-down-4">
+                                    <div class="item"><b>HARLEY DAVIDSON</b></div>
+                                    <div class="item"><b>KONFIO</b></div>
+                                    <div class="item"><b>LEVY HOLDING</b></div>
+                                    <div class="item"><b>LIVERPOOL</b></div>
+                                    <div class="item"><b>LVMH (Europe)</b></div>
+                                    <div class="item"><b>MABE</b></div>
+                                    <div class="item"><b>MERCK LATAM</b></div>
+                                    <div class="item"><b>MERCK CENTRO AMERICA & <br>CONO SUR</b></div>
+                                    <div class="item"><b>MSD</b></div>
                                     <div class="item"><b>NESTLÉ</b></div>
                                     <div class="item">Nescafé</div>
                                     <div class="item">Kit Kat</div>
@@ -51,9 +93,11 @@
                                     <div class="item">Corporate</div>
                                     <div class="space"></div>
                                 </div>
-                            </div>
-                            <div class="col-lg-2">
                                 <div class="animation-block reveal-down-4">
+
+
+                                </div>
+                                <div class="animation-block reveal-down-6">
                                     <div class="item"><b>NESTLÉ WATER</b></div>
                                     <div class="item">Perrier</div>
                                     <div class="item">S.Pellegrino</div>
@@ -61,78 +105,34 @@
                                     <div class="item">Sta María</div>
                                     <div class="space"></div>
                                 </div>
-                                <div class="animation-block reveal-down-4">
-                                    <div class="item"><b>DIAGEO</b></div>
-                                    <div class="item">Johnnie Walker</div>
-                                    <div class="item">Smirnoff</div>
-                                    <div class="item">Buchanan’s </div>
-                                    <div class="item">Baileys</div>
-                                    <div class="item">Black & White</div>
-                                    <div class="space"></div>
-                                </div>
-                                <div class="animation-block reveal-down-6">
-                                    <div class="item"><b>Church and Dwight</b></div>
-                                    <div class="item">Flawless</div>
-                                    <div class="item">Batiste</div>
-                                    <div class="item">Waterpick</div>
-                                    <div class="item">Sterimar</div>
-                                    <div class="space"></div>
-                                </div>
                             </div>
                             <div class="col-lg-2">
                                 <div class="animation-block reveal-down-6">
+                                    <div class="item"><b>PROCTER AND GAMBLE</b></div>
                                     <div class="item"><b>RBI</b></div>
                                     <div class="item">Popeyes</div>
                                     <div class="item">Burger King</div>
                                     <div class="space"></div>
+
                                 </div>
                                 <div class="animation-block reveal-down-2">
-                                    <div class="item"><b>Grupo Modelo</b></div>
-                                    <div class="item">Corona</div>
-                                    <div class="item">Cerveza Modelo</div>
-                                    <div class="item">Michelob Ultra</div>
-                                    <div class="item">Stella Artois</div>
-                                    <div class="item">Becks</div>
-                                    <div class="item">Michelob Seltzer</div>
-                                    <div class="item">Pacifico</div>
-                                    <div class="item">Victoria</div>
-                                    <div class="item">Z Tech</div>
-                                    <div class="item">Craft Beer Portfolio</div>
-                                    <div class="space"></div>
+                                    <div class="item"><b>SPLENTA</b></div>
+                                    <div class="item"><b>SALVATORE FERRAGAMO</b></div>
+                                    <div class="item"><b>SIX FLAGS</b></div>
+                                    <div class="item"><b>SOFITEL</b></div>
+                                    <div class="item"><b>TOUS</b></div>
                                 </div>
                             </div>
                             <div class="col-lg-2">
                                 <div class="animation-block reveal-down-12">
-                                    <div class="item"><b>SOFITEL</b></div>
-                                    <div class="item"><b>BMW</b></div>
-                                    <div class="item"><b>LEVY HOLDING</b></div>
-                                    <div class="item"><b>DECATHLON</b></div>
-                                    <div class="item"><b>MERCK</b></div>
-                                    <div class="item"><b>MSD</b></div>
-                                    <div class="item"><b>LVMH (Europe)</b></div>
-                                    <div class="item"><b>PROCTER AND GAMBLE</b></div>
                                 </div>
                             </div>
                             <div class="col-lg-2">
                                 <div class="animation-block reveal-down-11">
-                                    <div class="item"><b>LIVERPOOL</b></div>
-                                    <div class="item"><b>BAYER</b></div>
-                                    <div class="item"><b>TOUS</b></div>
-                                    <div class="item"><b>SALVATORE FERRAGAMO</b></div>
-                                    <div class="item"><b>HARLEY DAVIDSON</b></div>
-                                    <div class="item"><b>SIX FLAGS</b></div>
-                                    <div class="item"><b>MSD</b></div>
                                 </div>
                             </div>
                             <div class="col-lg-2">
                                 <div class="animation-block reveal-down-12">
-                                    <div class="item"><b>MERCK</b></div>
-                                    <div class="item"><b>MERCK CENTRO AMERICA & <br>CONO SUR</b></div>
-                                    <div class="item"><b>BAYER</b></div>
-                                    <div class="item"><b>KONFIO</b></div>
-                                    <div class="item"><b>FONDEADORA</b></div>
-                                    <div class="item"><b>MABE</b></div>
-                                    <div class="item"><b>SPLENTA</b></div>
                                 </div>
                             </div>
                         </div>
